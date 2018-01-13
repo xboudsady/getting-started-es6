6cdb17539ea682cd3d7f118b29738fdb4001f475
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-01-12 18:56:57 -0800

    First Commit

---
+++ es6-functions/04_functionality_the_same.js
@@ -0,0 +1,21 @@
+//Function Declaration
+
+function divide(a, b) {
+    return a / b;
+}
+
+//Function Expression
+
+const divide2 = function(a, b) {
+    return a / b;
+}
+
+//Arrow Function Expression
+
+const divide3 = (a, b) => {
+    return a / b;
+}
+
+//Arrow Function Expression - cocise
+
+const divide4 = (a, b) => a / b;
